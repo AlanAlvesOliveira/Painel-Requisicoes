87c70e5c44352e68396425a8b6690586fa91d5bb
AlanAlvesOliveira <37192828+AlanAlvesOliveira@users.noreply.github.com>
2023-06-24 16:48:32 -0300

    Adicionar arquivos de projeto.

---
+++ Painel-Requisicoes/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Authentication.Negotiate;
+
+namespace Painel_Requisicoes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+            builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+            .AddNegotiate();
+
+            builder.Services.AddAuthorization(options =>
+            {
+                // By default, all incoming requests will be authorized according to the default policy.
+                options.FallbackPolicy = options.DefaultPolicy;
+            });
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
